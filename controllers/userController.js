1f25d129a5aa62d930f44267ca61676bd8d2d804
leroy-netizen <leroythomas051@gmail.com>
2022-03-25 16:26:43 +0300

    register user

---
+++ controllers/userController.js
@@ -0,0 +1,69 @@
+const bcrypt = require('bcryptjs')
+const jwt = ('jsonwebtoken')
+const express = require('express')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+
+// @desc  Register new user
+// @route POST/api/users/
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password } = req.body //destructure from body data.
+
+    if(!name || !email ||!password){
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
+
+    //checking if user exists
+
+    const userExists = await User.findOne({email})
+
+        if(userExists){
+            res.status(400)
+            throw new Error('user already exists')
+        }
+
+        //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+    //Create a user 
+    const user = await User.create({
+    name, 
+    email,
+    password: hashedPassword
+}) 
+
+if(user){
+    res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email:user.email
+    })
+   } else {
+    res.status(400)
+    throw new Error("Invalid user data")
+}
+
+})
+// @desc  Authenticate a user
+// @route POST/api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+    res.json({message: 'Login User'})
+})
+// @desc  get User data
+// @route GET/api/users/me
+// @access Public
+const getMe = asyncHandler(async (req, res) => {
+    res.json({message: 'Load user data '})
+})
+
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe,
+}
--- models/goalModel.js
+++ models/goalModel.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose')
 
 const goalSchema = mongoose.Schema({
+    
+user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user'
+},
+    
     text: {
         type: String,
         required: [true, 'please add a text value']
