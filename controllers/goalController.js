e1881fd0aac648037c834cddd0cb054fe6a1fbb2
leroy-netizen <leroythomas051@gmail.com>
2022-03-25 12:44:15 +0300

    connecting with mongodb atlas

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -1,14 +1,16 @@
+const asyncHandler = require('express-async-handler')
+
 
 // @desc  Get goals
 // @route GET/api/goals
 // @access private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get goals' })
-}
+})
 // @desc  Add a goal
 // @route POST/api/goals
 // @access private
-const addGoal = (req, res) => {
+const addGoal = asyncHandler (async (req, res) => {
 
         if(!req.body.text){
             res.status(400)
@@ -17,24 +19,24 @@ const addGoal = (req, res) => {
     res.status(200).json({
         message: "Add goal"
     })
-}
+})
 // @desc  Update a goal
 // @route PUT/api/goals/:id
 // @access private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: `Update goal ${req.params.id}`
     })
-}
+})
 // @desc  Delete a goal
 // @route DELETE/api/goals/:id
 // @access private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: `Delete goal ${req.params.id}`
     })
 }
-
+)
 
 module.exports= {
     getGoals,
