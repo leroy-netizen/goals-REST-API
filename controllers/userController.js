0d97183cd55e9020b607bf2702d3e28105d6335c
leroy-netizen <leroythomas051@gmail.com>
2022-03-25 17:01:07 +0300

    user sign in with token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs')
-const jwt = ('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const express = require('express')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
@@ -27,7 +27,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
         //Hash password
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    const hashedPassword = await bcrypt.hash(password, salt)
 
     //Create a user 
     const user = await User.create({
@@ -40,7 +40,8 @@ if(user){
     res.status(201).json({
         _id: user.id,
         name: user.name,
-        email:user.email
+        email:user.email,
+        token: generateToken(user._id)
     })
    } else {
     res.status(400)
@@ -51,8 +52,25 @@ if(user){
 // @desc  Authenticate a user
 // @route POST/api/users/login
 // @access Public
+
+//LOging a user in
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message: 'Login User'})
+    const {email, password} = req.body
+
+     //check user email and match it to password
+    const user = await User.findOne({email})
+
+        if(user && (await bcrypt.compare(password, user.password))){
+             res.json({
+                _id: user.id,
+                name: user.name,
+                email:user.email,
+                token: generateToken(user._id)
+             })
+        } else {
+            res.status(400)
+            throw new Error("Invalid credentials")
+        }
 })
 // @desc  get User data
 // @route GET/api/users/me
@@ -61,6 +79,13 @@ const getMe = asyncHandler(async (req, res) => {
     res.json({message: 'Load user data '})
 })
 
+
+//generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '10d',
+    })
+}
 
 module.exports = {
     registerUser,
